b953425cc88e2a09f9600f280c14b97f790f5e70
Elue-dev <eluewisdom@gmail.com>
2021-07-25 17:36:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector ('.hamburger');
+menu.addEventListener('click', () => document.querySelector ('.mainmenu').classList.toggle ('show'));
+
+// window.addEventListener('scroll', () => {
+//     const nav = document.querySelector ('.mainmenu');
+//     nav.style.transform = 'translateX (-500px)';
+// });
